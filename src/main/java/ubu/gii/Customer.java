c4fb92a7aab58c811becac1439f87faa971a43c7
Estudiante <estudiante@EPV-AI-42A1-S00.eps.ubu.es>
2024-05-09 18:07:20 +0200

    #2 Remove Temp field - totalAmount

--- src/main/java/ubu/gii/Customer.java
+++ src/main/java/ubu/gii/Customer.java
@@ -44,13 +44,27 @@ public class Customer {
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
+			
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
+	}
+	
+	private double getTotalCharge(){
+		double result = 0;
+		
+		Iterator<Rental> rentals = _rentals.iterator();
+		while (rentals.hasNext()) {
+			
+			Rental each = rentals.next();
+			result += each.getCharge();
+		}
+		
+		return result;
+		
 	}
 	
 	
