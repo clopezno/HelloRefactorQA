931cc249af1241a9ddcabf2246ae3b4464b72eae
Estudiante <estudiante@EPV-AI-42A1-S00.eps.ubu.es>
2024-05-09 17:49:08 +0200

    #2 Move method - getFrequentRenterPoints to Rental

--- src/main/java/ubu/gii/Customer.java
+++ src/main/java/ubu/gii/Customer.java
@@ -42,7 +42,7 @@ public class Customer {
 			thisAmount = each.getCharge();
 			
 			// add frequent renter points
-			frequentRenterPoints += getFrequentRenterPoint(each);
+			frequentRenterPoints += each.getFrequentRenterPoint();
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(thisAmount) + "\n";
@@ -53,14 +53,5 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
-	}
-
-	private int getFrequentRenterPoint(Rental aRental) {
-		int frequentRenterPoints=1;
-		// add bonus for a two day new release rental
-		if ((aRental.getMovie().getPriceCode() == Movie.NEW_RELEASE)
-				&& aRental.getDaysRented() > 1)
-			frequentRenterPoints++;
-		return frequentRenterPoints;
 	}
 }
