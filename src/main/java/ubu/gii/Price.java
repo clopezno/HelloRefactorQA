e4d6bba7eb7aefed00c5ac74b9d34f11afe108a4
CARLOS LOPEZ NOZAL <clopezno@ubu.es>
2024-05-10 12:54:53 +0200

    #3 Remove  method Price.getCharge delagated

--- src/main/java/ubu/gii/Price.java
+++ src/main/java/ubu/gii/Price.java
@@ -3,9 +3,7 @@ package ubu.gii;
 public abstract class Price {
 	public abstract int getPriceCode();
 
-	public double getCharge(Movie movie, int daysRented) {
-		return getCharge(daysRented);
-	}
+	
 
 	public abstract double getCharge(int daysRented);
 }
