df8f634681ac53cacb9f95903592c968335d7aa0
CARLOS LOPEZ NOZAL <clopezno@ubu.es>
2024-05-10 12:52:22 +0200

    #3 Push down method Price.getCharge

--- src/main/java/ubu/gii/ChildrenPrice.java
+++ src/main/java/ubu/gii/ChildrenPrice.java
@@ -8,4 +8,13 @@ public class ChildrenPrice extends Price {
 		return  Movie.CHILDRENS;
 	}
 
+	@Override
+	public double getCharge(int daysRented) {
+		double result=1.5;
+
+		if (daysRented > 3)
+			result += (daysRented- 3) * 1.5;
+		return result;
+	}
+
 }
--- src/main/java/ubu/gii/NewReleasePrice.java
+++ src/main/java/ubu/gii/NewReleasePrice.java
@@ -8,4 +8,10 @@ public class NewReleasePrice extends Price {
 		return Movie.NEW_RELEASE;
 	}
 
+	@Override
+	public double getCharge(int daysRented) {
+
+		return  daysRented * 3;
+	}
+
 }
