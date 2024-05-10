57746a8e8ae7ab0ab210912e6a95702851f6e1d8
CARLOS LOPEZ NOZAL <clopezno@ubu.es>
2024-05-10 12:43:02 +0200

    #3 Move method Movie.getCharge to Price

--- src/main/java/ubu/gii/Movie.java
+++ src/main/java/ubu/gii/Movie.java
@@ -50,23 +50,7 @@ public class Movie {
 	}
 
 	public double getCharge(int daysRented) {
-		double result=0;
-		switch (getPriceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (daysRented> 2)
-				result += (daysRented - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			result += daysRented * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (daysRented > 3)
-				result += (daysRented- 3) * 1.5;
-			break;
-		}
-		return result;
+		return _priceCode.getCharge(daysRented);
 	}
 
 	public int getFrequentRenterPoint(int daysRented) {
