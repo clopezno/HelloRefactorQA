5101f9e1057789f8140a69abb504dafbffd3e065
Javier <javierpampliega2002@gmail.com>
2024-05-15 10:57:35 +0200

    Refactor Move Method (determineAmountsForEachMovie) to Rental Class

--- src/main/java/ubu/gii/Customer.java
+++ src/main/java/ubu/gii/Customer.java
@@ -38,7 +38,7 @@ public class Customer {
 		while (rentals.hasNext()) {
 			double thisAmount = 0;
 			Rental each = rentals.next();
-			thisAmount = determineAmountsForEachMovie(thisAmount, each);
+			thisAmount = each.determineAmountsForEachMovie(thisAmount);
 			
 			// add frequent renter points
 			frequentRenterPoints++;
@@ -56,25 +56,5 @@ public class Customer {
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
-	}
-
-	private double determineAmountsForEachMovie(double thisAmount, Rental each) {
-		// determine amounts for each line
-		switch (each.getMovie().getPriceCode()) {
-		case Movie.REGULAR:
-			thisAmount += 2;
-			if (each.getDaysRented() > 2)
-				thisAmount += (each.getDaysRented() - 2) * 1.5;
-			break;
-		case Movie.NEW_RELEASE:
-			thisAmount += each.getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			thisAmount += 1.5;
-			if (each.getDaysRented() > 3)
-				thisAmount += (each.getDaysRented() - 3) * 1.5;
-			break;
-		}
-		return thisAmount;
 	}
 }
